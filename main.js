c1a8fa048e0b5716934937075a93674a69619152
Clement-Julia <81628168+Clement-Julia@users.noreply.github.com>
2023-01-06 09:08:18 +0100

    fix: optimisation userpage

--- main.js
+++ main.js
@@ -1,35 +1,26 @@
 import './style.css'
-import Card from "./src/components/Card";
 import UserPage from "./src/pages/UserPage";
 import TabManager from "./src/utils/TabManager";
 
-const rootElement = document.querySelector('#app')
+const rootElement = document.querySelector('#app');
+const select = document.querySelector('#select-page');
 
 const tabManager = new TabManager(rootElement, {
-  page1: {
-    component: () => document.createElement('div'),
-    params: [{ src: 'http://placekitten.com/200/200', text: 'A cat' }]
-  },
   user: {
-    component: UserPage,
-    params: [{ page: 1 }]
+    component: UserPage
   }
 })
 
-tabManager.openTabById('user')
-
-document.querySelectorAll('[data-tabId]').forEach(element => {
-  element.addEventListener('click', () => {
-    tabManager.openTabById(element.getAttribute('data-tabId'))
-  })
-})
+tabManager.openTabById('user', [{ page: 1 }])
 
 document.querySelectorAll('[data-page]').forEach(element => {
   element.addEventListener('click', () => {
-    tabManager.openTabById('user', element.getAttribute('data-page'));
+    select.value = parseInt(select.value) + parseInt(element.getAttribute('data-page'));
+    select.dispatchEvent(new Event('change'));
   })
+
 })
 
 document.querySelector('#select-page').addEventListener('change', (e) => {
-  tabManager.openTabById('user', e.target.value);
+  tabManager.openTabById('user', [{ page: e.target.value }]);
 })
